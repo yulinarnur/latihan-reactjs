54abd98306a3c96183b3183902061f04dece4b42
YulinarNR <yulinarnurrahma@gmail.com>
2023-12-31 01:28:56 +0700

    typewrite-text

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import Particles from "react-particles";
 import { loadFireworksPreset } from "tsparticles-preset-fireworks";
+import { Typewriter } from "react-simple-typewriter";
+import { useState } from "react";
 
 function App() {
+  const [newYearMessage, setNewYearMessage] = useState(["Bye 2023!"]);
+
   const particleInitialization = async (engine) => {
     await loadFireworksPreset(engine);
   };
@@ -11,6 +15,16 @@ function App() {
         init={particleInitialization}
         options={{ preset: "fireworks" }}
       />
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <span className="text-white text-4xl font-bold z-50">
+          <Typewriter
+            words={newYearMessage}
+            loop={false}
+            cursorStyle={"_"}
+            cursor
+          />
+        </span>
+      </div>
     </>
   );
 }
